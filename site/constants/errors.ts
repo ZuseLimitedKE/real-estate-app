d9200e533b8e9391be09afc1ef01276740812d5d
mbeka02 <antonymbeka@gmail.com>
2025-09-03 21:29:43 +0300

    fix:fixed grammartical errors in error messages

--- site/constants/errors.ts
+++ site/constants/errors.ts
@@ -6,8 +6,8 @@ export class MyError extends Error {
 }
 
 export enum Errors {
-  NOT_ADD_PROPERTY = "Could not add property the database",
+  NOT_ADD_PROPERTY = "Could not add property to the database",
   NOT_GET_PROPERTIES = "Could not get properties",
-  NOT_ADD_AGENCY = "Could not add agency the database",
+  NOT_ADD_AGENCY = "Could not add agency to the database",
   NOT_GET_AGENCIES = "Could not get agencies",
 }
