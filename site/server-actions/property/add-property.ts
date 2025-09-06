21aefeb4f6c47c69fd76a8bb752a0c68e8f4a47c
Roman Njoroge <roman.njoroge@njuguna.com>
2025-09-06 16:16:51 +0300

    feat: Emitting token id, and giving the ABI the correct name

--- site/server-actions/property/add-property.ts
+++ site/server-actions/property/add-property.ts
@@ -2,6 +2,7 @@
 import database from "@/db";
 import { MyError, Errors } from "@/constants/errors";
 import { Properties } from "@/db/collections";
+
 export async function AddProperty(FormData: Properties) {
   //TODO: There has to be an auth check here , not all users should be able to add properties
   try {
--- site/smartcontract/registerContract.ts
+++ site/smartcontract/registerContract.ts
@@ -1,5 +1,5 @@
 import {Web3} from 'web3';
-import abi from "../realEstaetManagerABI.json";
+import abi from "../realEstateManagerABI.json";
 
 export interface RegisterPropertyContract {
     propertyID: string,
@@ -12,9 +12,17 @@ export interface RegisterPropertyContract {
     serviceFee: number
 }
 
-const web3 = new Web3(process.env.HEDERA_RPC_URL!);
-const account = web3.eth.accounts.wallet.add(process.env.HEDERA_ACCOUNT!);
-const contract = new web3.eth.Contract(abi.abi, process.env.REAL_ESTATE_MANAGER_CONTRACT!);
+const rpcURL = process.env.HEDERA_RPC_URL;
+const pkEnv = process.env.HEDERA_ACCOUNT;
+const contractAddress = process.env.REAL_ESTATE_MANAGER_CONTRACT;
+
+if (!rpcURL || !pkEnv || !contractAddress) {
+    throw new Error("Invalid environment variable set up, set HEDERA_RPC_URL, HEDERA_ACCOUNT and REAL_ESTATE_MANAGER_CONTRACT in env variables");
+}
+
+const web3 = new Web3(rpcURL);
+const account = web3.eth.accounts.wallet.add(pkEnv);
+const contract = new web3.eth.Contract(abi.abi, contractAddress);
 
 class RealEstateManagerContract {
     async register(args: RegisterPropertyContract): Promise<{tokenID: string | undefined, txHash: string}> {
@@ -30,12 +38,12 @@ class RealEstateManagerContract {
                 BigInt(args.serviceFee)
             ).send({from: account[0].address});
 
-            const tokenID = tx.events?.PropertyRegistered?.returnValues?.id
+            const tokenID = tx.events?.PropertyRegistered?.returnValues?.tokenID;
 
             return {txHash: tx.transactionHash, tokenID: tokenID ? tokenID as string : undefined};
         } catch(err) {
             console.error("Error registering property in contract", err);
-            throw new Error("Error registering contract");
+            throw new Error(`Error registering contract: ${(err as Error).message}`);
         }
     }
 }
