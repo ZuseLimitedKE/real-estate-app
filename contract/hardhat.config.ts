38f6cc9753a95fd83c21c06c8f0d2758565ed1d8
Roman Njoroge <roman.njoroge@njuguna.com>
2025-09-06 12:39:26 +0300

    feat: Deploying contract

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,10 +1,13 @@
 import type { HardhatUserConfig } from "hardhat/config";
 
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import hardhatVerify from "@nomicfoundation/hardhat-verify";
 import { configVariable } from "hardhat/config";
 
+const ETHERSCAN_API_KEY = configVariable("ETHERSCAN_API_KEY");
+
 const config: HardhatUserConfig = {
-  plugins: [hardhatToolboxMochaEthersPlugin],
+  plugins: [hardhatToolboxMochaEthersPlugin, hardhatVerify],
   solidity: {
     profiles: {
       default: {
@@ -27,6 +30,11 @@ const config: HardhatUserConfig = {
       url: configVariable("HEDERA_RPC_URL"),
       accounts: [configVariable("HEDERA_PRIVATE_KEY")]
     },
+    hedera_testnet: {
+      type: "http",
+      url: configVariable("HEDERA_TESTNET_RPC_URL"),
+      accounts: [configVariable("HEDERA_TESTNET_PRIVATE_KEY")]
+    },
     hardhatMainnet: {
       type: "edr-simulated",
       chainType: "l1",
@@ -40,6 +48,11 @@ const config: HardhatUserConfig = {
       chainType: "l1",
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+  },
+  verify: {
+    etherscan: {
+      apiKey: ETHERSCAN_API_KEY,
     },
   },
 };
