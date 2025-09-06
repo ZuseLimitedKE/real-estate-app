4e6596a12c5ffe0fc2d0f3a191786d5f5b0443ab
mbeka02 <antonymbeka@gmail.com>
2025-09-06 14:48:18 +0300

    new types

---
+++ site/types/form.ts
@@ -0,0 +1,24 @@
+import { ZodType } from "zod";
+import { LucideIcon } from "lucide-react";
+import type { AddPropertyFormData } from "./property";
+
+type FieldKeys = keyof AddPropertyFormData;
+
+export type FormStep = {
+  title: string;
+  position: number;
+  validationSchema: ZodType<unknown>;
+  component: React.ReactElement;
+  icon: LucideIcon;
+  fields: FieldKeys[];
+};
+export interface MultiStepFormContextProps {
+  currentStep: FormStep;
+  currentStepIndex: number;
+  isFirstStep: boolean;
+  isLastStep: boolean;
+  nextStep: () => void;
+  previousStep: () => void;
+  goToStep: (step: number) => void;
+  steps: FormStep[];
+}
