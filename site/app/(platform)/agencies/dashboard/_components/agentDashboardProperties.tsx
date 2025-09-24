67bcf7c1de0efd72733d87b6c34657ec9685b62a
mbeka02 <antonymbeka@gmail.com>
2025-09-24 07:12:36 +0300

    updated

--- site/app/(platform)/agencies/dashboard/_components/agentDashboardProperties.tsx
+++ site/app/(platform)/agencies/dashboard/_components/agentDashboardProperties.tsx
@@ -1,5 +1,3 @@
-"use server";
-
 import PaginationControls from "@/components/paginationControls";
 import { Button } from "@/components/ui/button";
 import getDashboardProperties from "@/server-actions/agent/dashboard/getProperties";
@@ -7,88 +5,91 @@ import { Eye, MapPin, Pencil, Trash } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function AgentDashboardProperties(props: { page: number }) {
-    const {properties, totalPages} = await getDashboardProperties(props.page);
+export default async function AgentDashboardProperties(props: {
+  page: number;
+}) {
+  const { properties, totalPages } = await getDashboardProperties(props.page);
 
-    const propertiesListItems = properties.map((p) => {
-        const detailItems = p.details.map((d, index) => (
-            <p key={index}>
-                <span className="font-bold text-sm">{d.title}: </span>
-                <span>{d.value}</span>
-            </p>
-        ));
-
-        return (
-            <li key={p.id}>
-                <section className="flex flex-col gap-2 rounded-md border border-solid border-slate-300">
-                    <header>
-                        <div className="bg-slate-200">
-                            <Image
-                                src={p.image}
-                                alt={p.name}
-                                width={500}
-                                height={500}
-                                className="max-w-full object-cover lg:mx-auto"
-                            />
-                        </div>
-
-                        <div className="flex flex-row gap-2 flex-wrap p-4 items-center">
-                            <p className="text-lg font-bold">{p.name}</p>
-                            <div className="px-2 py-1 rounded-full bg-primary text-white font-bold text-xs">
-                                <p>{p.status}</p>
-                            </div>
-                        </div>
-                    </header>
-
-                    <div className="px-4 pb-4 flex flex-col gap-2">
-                        <div className="flex flex-row gap-1 flex-wrap items-center">
-                            <MapPin className="w-4 h-4" />
-                            <p className="text-slate-500">{p.location}</p>
-                        </div>
-
-                        <div className="flex flex-col gap-1 lg:flex-row lg:flex-wrap lg:gap-4">
-                            {detailItems}
-                        </div>
-
-                        <p className="font-bold text-lg">KSh {p.rent}/month</p>
-                        <footer className="flex flex-col lg:flex-row gap-1">
-                            <div className="flex flex-row gap-2 flex-wrap">
-                                <Button className="flex-1" variant='outline' asChild>
-                                    <Link href={`/agencies/properties/${p.id}`}>
-                                        <Eye />
-                                        <p>View</p>
-                                    </Link>
-                                </Button>
-                                <Button className="flex-1" variant='outline'>
-                                    <Pencil />
-                                    <p>Edit</p>
-                                </Button>
-                            </div>
-
-                            <Button className="lg:flex-1" variant='destructive'>
-                                <Trash />
-                                <p>Delete</p>
-                            </Button>
-                        </footer>
-                    </div>
-
-                </section>
-            </li>
-        );
-    });
+  const propertiesListItems = properties.map((p) => {
+    const detailItems = p.details.map((d, index) => (
+      <p key={index}>
+        <span className="font-bold text-sm">{d.title}: </span>
+        <span>{d.value}</span>
+      </p>
+    ));
 
     return (
-        <article>
-            <header className="flex flex-row justify-between my-4">
-                <h2 className="font-bold text-2xl">My Properties</h2>
-                <p className="text-slate-400">{properties.length} properties</p>
-            </header>
-            <ul className="flex flex-col gap-4">
-                {propertiesListItems}
-            </ul>
-            <div className="mt-4 flex flex-row justify-center">
-                <PaginationControls currentPage={props.page} totalPages={totalPages} paramName="propertiesPage"/>
+      <li key={p.id}>
+        <section className="flex flex-col gap-2 rounded-md border border-solid border-slate-300">
+          <header>
+            <div className="bg-slate-200">
+              <Image
+                src={p.image}
+                alt={p.name}
+                width={500}
+                height={500}
+                className="max-w-full object-cover lg:mx-auto"
+              />
             </div>
-        </article>
+
+            <div className="flex flex-row gap-2 flex-wrap p-4 items-center">
+              <p className="text-lg font-bold">{p.name}</p>
+              <div className="px-2 py-1 rounded-full bg-primary text-white font-bold text-xs">
+                <p>{p.status}</p>
+              </div>
+            </div>
+          </header>
+
+          <div className="px-4 pb-4 flex flex-col gap-2">
+            <div className="flex flex-row gap-1 flex-wrap items-center">
+              <MapPin className="w-4 h-4" />
+              <p className="text-slate-500">{p.location}</p>
+            </div>
+
+            <div className="flex flex-col gap-1 lg:flex-row lg:flex-wrap lg:gap-4">
+              {detailItems}
+            </div>
+
+            <p className="font-bold text-lg">KSh {p.rent}/month</p>
+            <footer className="flex flex-col lg:flex-row gap-1">
+              <div className="flex flex-row gap-2 flex-wrap">
+                <Button className="flex-1" variant="outline" asChild>
+                  <Link href={`/agencies/properties/${p.id}`}>
+                    <Eye />
+                    <p>View</p>
+                  </Link>
+                </Button>
+                <Button className="flex-1" variant="outline">
+                  <Pencil />
+                  <p>Edit</p>
+                </Button>
+              </div>
+
+              <Button className="lg:flex-1" variant="destructive">
+                <Trash />
+                <p>Delete</p>
+              </Button>
+            </footer>
+          </div>
+        </section>
+      </li>
     );
+  });
+
+  return (
+    <article>
+      <header className="flex flex-row justify-between my-4">
+        <h2 className="font-bold text-2xl">My Properties</h2>
+        <p className="text-slate-400">{properties.length} properties</p>
+      </header>
+      <ul className="flex flex-col gap-4">{propertiesListItems}</ul>
+      <div className="mt-4 flex flex-row justify-center">
+        <PaginationControls
+          currentPage={props.page}
+          totalPages={totalPages}
+          paramName="propertiesPage"
+        />
+      </div>
+    </article>
+  );
 }
--- site/app/(platform)/agencies/dashboard/_components/agentDashboardStatisticsItem.tsx
+++ site/app/(platform)/agencies/dashboard/_components/agentDashboardStatisticsItem.tsx
@@ -1,19 +1,19 @@
-import type { ReactNode } from "react";
-
 interface AgentDashboardStatisticsData {
-    title: string;
-    icon: ReactNode;
-    value: string;
+  title: string;
+  icon: React.ReactNode;
+  value: string;
 }
 
-export default function AgentDashboardStatisticsItem(props: AgentDashboardStatisticsData) {
-    return (
-        <section className="p-4 border border-solid rounded-md border-slate-300">
-            <header className="flex flex-row justify-between mb-2 font-bold text-sm">
-                <h3>{props.title}</h3>
-                {props.icon}
-            </header>
-            <p>{props.value}</p>
-        </section>
-    );
+export default function AgentDashboardStatisticsItem(
+  props: AgentDashboardStatisticsData,
+) {
+  return (
+    <section className="p-4 border border-solid rounded-md border-slate-300">
+      <header className="flex flex-row justify-between mb-2 font-bold text-sm">
+        <h3>{props.title}</h3>
+        {props.icon}
+      </header>
+      <p>{props.value}</p>
+    </section>
+  );
 }
