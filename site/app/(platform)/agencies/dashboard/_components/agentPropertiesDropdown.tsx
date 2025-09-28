a8bae656c94c4d5c70973dd8ead95c514c799e34
mbeka02 <antonymbeka@gmail.com>
2025-09-28 14:57:35 +0300

    feat: agents can now delete listed properties

--- site/app/(platform)/agencies/dashboard/_components/agentPropertiesDropdown.tsx
+++ site/app/(platform)/agencies/dashboard/_components/agentPropertiesDropdown.tsx
@@ -6,9 +6,26 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { MyError } from "@/constants/errors";
+import { DeleteAgencyProperty } from "@/server-actions/agent/dashboard/deleteProperty";
 import { MoreHorizontal, Eye, Pencil, Trash } from "lucide-react";
 import Link from "next/link";
+import { toast } from "sonner";
 export function AgentPropertiesDropdown({ id }: { id: string }) {
+  const handleDeleteProperty = async () => {
+    try {
+      await DeleteAgencyProperty(id);
+      toast.success("The property has been deleted");
+    } catch (err) {
+      console.error(err);
+      if (err instanceof MyError) {
+        toast.error(err.message);
+      } else {
+        toast.error("unable to delete the property");
+      }
+    }
+  };
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -36,7 +53,10 @@ export function AgentPropertiesDropdown({ id }: { id: string }) {
           <Pencil className="h-4 w-4" />
           Edit
         </DropdownMenuItem>
-        <DropdownMenuItem className="flex items-center gap-2 text-destructive/80 hover:text-destructive hover:bg-slate-50">
+        <DropdownMenuItem
+          className="flex items-center gap-2 text-destructive/80 hover:text-destructive hover:bg-slate-50"
+          onClick={handleDeleteProperty}
+        >
           <Trash className="h-4 w-4" />
           Delete
         </DropdownMenuItem>
