510c90eb716ae2ba458ee822269afda1d5911584
Roman Njoroge <roman.njoroge@njuguna.com>
2025-10-15 12:49:41 +0300

    feat: MultiStepForm now using current form gotten from steps and currentStep state

--- site/app/(platform)/agencies/registerRoman/_components/step-form.tsx
+++ site/app/(platform)/agencies/registerRoman/_components/step-form.tsx
@@ -22,49 +22,94 @@ interface CreatePropertyContextType {
     type: PropertyType | null;
     setCurrentStep: (num: number) => void,
     saveFormState: (step: number) => void,
+    steps: Steps[]
 }
 
 interface Steps {
     num: number,
-    item: React.ReactElement
+    item: React.ReactElement,
+    title: string
 }
+
+const apartmentSteps: Steps[] = [
+    {
+        num: 1,
+        item: <CreatePropertyStep1Form />,
+        title: "Select property type"
+    },
+    {
+        num: 2,
+        item: <ApartmentEstateDetailsForm />,
+        title: "Apartment Estate Details"
+    },
+    {
+        num: 3,
+        item: <ApartmentDocumentsForm />,
+        title: "Apartment Documents"
+    },
+    {
+        num: 4,
+        item: <ApartmentUnitTemplatesForm />,
+        title: "Apartment Unit Templates"
+    },
+    {
+        num: 5,
+        item: <ApartmentUnitsForm />,
+        title: "Apartment Units"
+    }
+];
+
+const singlePropertySteps: Steps[] = [
+
+]
 
 export const CreatePropertyContext = createContext<CreatePropertyContextType | null>(null);
 
 export default function MultiStepForm({ userID }: MultiStepFormProps) {
-    const [currentStep, setCurrentStep] = useState<number>(1);
+    const [currentStep, setCurrentStep] = useState<number>(0);
     const [propertyType, setPropertyType] = useState<PropertyType | null>(null);
 
     const saveFormState = (step: number) => {
         console.log(step);
     }
 
+    const steps = propertyType === null ? [{
+        num: 1,
+        item: <CreatePropertyStep1Form />,
+        title: "Select property type"
+    }] : propertyType === PropertyType.APARTMENT ? apartmentSteps : singlePropertySteps;
+
     const value: CreatePropertyContextType = {
         currentStep,
         type: propertyType,
         setCurrentStep,
-        saveFormState
+        saveFormState,
+        steps
     }
 
     const form = useForm<z.infer<typeof createPropertySchema>>({
         resolver: zodResolver(createPropertySchema)
     })
 
+    const currentForm = steps.find((s) => s.num === currentStep + 1);
+
     return (
         <CreatePropertyContext value={value}>
             <FormProvider {...form}>
-                <form className="space-y-6 max-w-4xl mx-auto p-6">
-                    <Card>
-                        <CardHeader>
-                            {/* Progress indicator */}
-                            <CardTitle>{"Test Title"}</CardTitle>
-                        </CardHeader>
-                        <CardContent className="space-y-4">
-                            {<ApartmentUnitsForm /> }
-                            {/* Page controls */}
-                        </CardContent>
-                    </Card>
-                </form>
+                {currentForm && (
+                    <form className="space-y-6 max-w-4xl mx-auto p-6">
+                        <Card>
+                            <CardHeader>
+                                {/* Progress indicator */}
+                                <CardTitle>{currentForm.title}</CardTitle>
+                            </CardHeader>
+                            <CardContent className="space-y-4">
+                                {currentForm.item}
+                                {/* Page controls */}
+                            </CardContent>
+                        </Card>
+                    </form>
+                )}
             </FormProvider>
         </CreatePropertyContext>
     );
