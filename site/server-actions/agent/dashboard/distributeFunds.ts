191743284a5e2509096a7adb387daeb3935a4b1f
Roman Njoroge <roman.njoroge@njuguna.com>
2025-10-25 20:04:02 +0300

    feat: Server action for sending fund

---
+++ site/server-actions/agent/dashboard/distributeFunds.ts
@@ -0,0 +1,22 @@
+"use server"
+
+import { requireRole } from "@/auth/utils";
+import { MyError } from "@/constants/errors";
+import realEstateManagerContract from "@/smartcontract/registerContract";
+import { DistributePropertyInvestor } from "@/types/property_details";
+
+export default async function distributeFund(investor: DistributePropertyInvestor, totalAmount: number, decimals: number, tokenAddress: string) {
+    try {
+        await requireRole("agency");
+
+        // Parse details to send to registerContract
+        await realEstateManagerContract.distributeFund(
+            investor.walletAddress,
+            (investor.percentage / 100) * totalAmount * (10**decimals),
+            tokenAddress
+        )
+    } catch(err) {
+        console.error("Could not send investor their portion of funds", err);
+        throw new MyError("Could not send investor their portion of funds");
+    }
+}
