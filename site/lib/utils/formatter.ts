378f2cf6f4af31dcc29077162976a9e8a49cb263
Roman Njoroge <roman.njoroge@njuguna.com>
2025-10-27 21:23:51 +0300

    feat: Formatting hedera addresses differently

--- site/lib/utils/formatter.ts
+++ site/lib/utils/formatter.ts
@@ -1,5 +1,9 @@
 export function formatAddress(address: string): string {
-    const front = address.slice(0, 6);
-    const end = address.slice(-3);
-    return `${front}...${end}`;
+    if (address.startsWith('0x')) {
+        const front = address.slice(0, 6);
+        const end = address.slice(-3);
+        return `${front}...${end}`;
+    } else {
+        return address
+    }
 }
