62826a12551cf5c8e89265fad8fc65d893c156ba
Roman Njoroge <roman.njoroge@njuguna.com>
2025-10-31 22:05:35 +0300

    feat: Removing unused contract files

--- contract/ignition/modules/RealEstateManager.ts
+++
@@ -1,6 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("RealEstateManagerModule", (m) => {
-  const realEstate = m.contract("RealEstateManager");
-  return { realEstate };
-});
